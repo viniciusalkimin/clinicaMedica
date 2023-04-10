d28ce7b242cdcf19bc0634174b2acc863b1629b9
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-04-10 18:41:29 -0300

    Implementação da geração de Token JWT com lib Auth0

--- src/main/java/br/com/alkimin/apiclinicamedica/controller/AutenticacaoController.java
+++ src/main/java/br/com/alkimin/apiclinicamedica/controller/AutenticacaoController.java
@@ -1,7 +1,10 @@
 package br.com.alkimin.apiclinicamedica.controller;
 
 import br.com.alkimin.apiclinicamedica.domain.models.DadosAtutenticacao;
+import br.com.alkimin.apiclinicamedica.domain.models.Usuario;
+import br.com.alkimin.apiclinicamedica.infra.security.TokenService;
 import br.com.alkimin.apiclinicamedica.service.AutenticacaoService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,12 +17,13 @@ import org.springframework.web.bind.annotation.*;
 public class AutenticacaoController {
 
     private AuthenticationManager authenticationManager;
+    private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody DadosAtutenticacao dados) {
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAtutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var auth = authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) auth.getPrincipal()));
     }
 
 
---
+++ src/main/java/br/com/alkimin/apiclinicamedica/infra/security/TokenService.java
@@ -0,0 +1,34 @@
+package br.com.alkimin.apiclinicamedica.infra.security;
+
+import br.com.alkimin.apiclinicamedica.domain.models.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("senhaSecreta");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("erro ao gerrar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
