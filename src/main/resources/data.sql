ae89b7ae3d73f8ee7787c2876958820fc652638a
vinicius.alkimin <viniciusg_alkimin@hotmail.com>
2023-04-09 23:00:32 -0300

    Add classe para autenticacao e tranformando em UserDatails para setar como objeto de auth

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+/*INSERT INTO usuarios(login, senha) VALUES ('vinicius@vollmed.com', '123456');
+INSERT INTO usuarios(login, senha) VALUES ('davi@vollmed.com', '123456');*/
+
+INSERT INTO usuarios(login, senha) VALUES('vinicius@vollmed','$2a$12$XtMG7JUGqelUO04Us9FcQuB.G2.RZv27pF50KY7NNToFHonXLNmty');
