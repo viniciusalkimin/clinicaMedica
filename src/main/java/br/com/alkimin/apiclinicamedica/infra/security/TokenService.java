7efe49b0fb677e03115b827849fb92a9e6e6b09c
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-04-13 18:10:22 -0300

    Criando filter e recuperando token da request

--- src/main/java/br/com/alkimin/apiclinicamedica/infra/security/TokenService.java
+++ src/main/java/br/com/alkimin/apiclinicamedica/infra/security/TokenService.java
@@ -5,11 +5,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
